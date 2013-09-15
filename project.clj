03c0412d411865705adfc09d333e5da696106b94
Scott Rabin <scottrabin@gmail.com>
2013-09-15 15:19:21 -0700

    Initial project commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject MiniMC "0.1.0-SNAPSHOT"
+  :description "Web interface to act as a remote for XBMC"
+  :license {:name "GPL v2"
+            :url "http://www.gnu.org/licenses/gpl-2.0.txt"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
